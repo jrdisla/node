0999d32a5d94cee1b5390112d1736c0426a98274
Julio Disla <20111614@ce.pucmm.edu.do>
2019-06-28 14:19:46 +0000

    second commit

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hola Mundo!');
 });
 
 app.listen(3000, function () {
